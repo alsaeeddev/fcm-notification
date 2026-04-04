ef3db5789b1aacf8f549499f86a3e2740e17e4a8
Al Saeed <alsaeeddev@gmail.com>
2026-04-04 15:53:37 +0500

    Undo Deleted

--- index.js
+++ index.js
@@ -105,10 +105,6 @@ app.post("/send-notification", authenticate, async (req, res) => {
 
     console.log("✅ Notification sent:", response);
 
-    await db.collection("notifications").doc(noteId).delete();
-
-    console.log("🗑️ Notification document deleted:", noteId);
-
     return res.json({
       success: true,
       fcmResponse: response,
